4bd8845573d8a6f329f6aded5e82a1f3cc4d4db3
quachnam95 <52250955+quachnam95@users.noreply.github.com>
2020-09-08 14:33:11 +0900

    Update index.js

--- [20200609]The-Web-socket-io-and-Connection-DB/index.js
+++ [20200609]The-Web-socket-io-and-Connection-DB/index.js
@@ -127,5 +127,15 @@ io.sockets.on('connection', function (socket) {
         }
 
     })
+    var myPythonScriptPath = '/mnt/c/Users/Admin/Desktop/web-socket-io-mysql-master/finding_lst_paths.py';
+    const {PythonShell} = require("python-shell");
 
+    // Use python shell
+    //var PythonShell = require('python-shell');
+    var pyshell = new PythonShell(myPythonScriptPath);
+
+    pyshell.on('message', function (message) {
+        // received a message sent from the Python script (a simple "print" statement)
+        console.log(message);
+    });
 })
